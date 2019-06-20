98510ebc75c0d84acbd0c38bca0a5bac5200899e
seholee <shlee@fastfive.co.kr>
2019-06-20 17:26:19 +0900

    make home

---
+++ src/Detail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Query } from "react-apollo";
+
+const Detail = ({
+  match: {
+    params: { movieId }
+  }
+}) => {
+  console.log(movieId);
+  return <div>Detail</div>;
+};
+
+export default Detail;
